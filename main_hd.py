faecfe872d09d4087ce01e868894bb9a6c6d727f
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-05-02 02:06:44 +0800

    Create main_hd.py

---
+++ main_hd.py
@@ -0,0 +1,23 @@
+from model import *
+
+if __name__ == '__main__':
+    dataset_seq = [1, 2, 3, 4, 5]
+    prod_seq = [1, 2]
+    cm_seq = [1, 2]
+    wd_seq = [1, 2]
+    for data_setting in dataset_seq:
+        dataset_name = 'email_undirected' * (data_setting == 1) + 'dnc_email_directed' * (data_setting == 2) + 'email_Eu_core_directed' * (data_setting == 3) + \
+                       'WikiVote_directed' * (data_setting == 4) + 'NetPHY_undirected' * (data_setting == 5)
+        for cm in cm_seq:
+            cascade_model = 'ic' * (cm == 1) + 'wc' * (cm == 2)
+            for prod_setting in prod_seq:
+                product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
+
+                Model(dataset_name, product_name, cascade_model).model_hd()
+                Model(dataset_name, product_name, cascade_model).model_hed()
+
+                for wd in wd_seq:
+                    wallet_distribution_type = 'm50e25' * (wd == 1) + 'm99e96' * (wd == 2)
+
+                    ModelPW(dataset_name, product_name, cascade_model, wallet_distribution_type).model_hdpw()
+                    ModelPW(dataset_name, product_name, cascade_model, wallet_distribution_type).model_hedpw()
