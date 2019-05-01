d8554c954eaac12591d7d29bfb4f52b1e991bed3
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-05-02 02:06:55 +0800

    Create main_pmis.py

---
+++ main_pmis.py
@@ -0,0 +1,16 @@
+from model import *
+
+if __name__ == '__main__':
+    dataset_seq = [1, 2, 3, 4, 5]
+    prod_seq = [1, 2]
+    cm_seq = [1, 2]
+    wd_seq = [1, 2]
+    for data_setting in dataset_seq:
+        dataset_name = 'email_undirected' * (data_setting == 1) + 'dnc_email_directed' * (data_setting == 2) + 'email_Eu_core_directed' * (data_setting == 3) + \
+                       'WikiVote_directed' * (data_setting == 4) + 'NetPHY_undirected' * (data_setting == 5)
+        for cm in cm_seq:
+            cascade_model = 'ic' * (cm == 1) + 'wc' * (cm == 2)
+            for prod_setting in prod_seq:
+                product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
+
+                Model(dataset_name, product_name, cascade_model).model_pmis()
