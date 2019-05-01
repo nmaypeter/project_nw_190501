1b82fa9e95a6f6c35137ab659e9f621118186de3
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-05-02 02:07:13 +0800

    Create SeedSelection_PMIS.py

---
+++ SeedSelection_PMIS.py
@@ -0,0 +1,39 @@
+from Diffusion import *
+import operator
+
+
+class SeedSelectionPMIS:
+    def __init__(self, g_dict, s_c_dict, prod_list, monte):
+        ### g_dict: (dict) the graph
+        ### s_c_dict: (dict) the set of cost for seeds
+        ### prod_list: (list) the set to record products [kk's profit, kk's cost, kk's price]
+        ### num_node: (int) the number of nodes
+        ### num_product: (int) the kinds of products
+        ### monte: (int) monte carlo times
+        self.graph_dict = g_dict
+        self.seed_cost_dict = s_c_dict
+        self.product_list = prod_list
+        self.num_node = len(s_c_dict)
+        self.num_product = len(prod_list)
+        self.monte = monte
+
+    def generateCelfHeap(self):
+        # -- calculate expected profit for all combinations of nodes and products --
+        ### celf_item: (list) (mg, k_prod, i_node, flag)
+        celf_heap = [(0.0, -1, '-1', 0)]
+
+        diff_ss = Diffusion(self.graph_dict, self.seed_cost_dict, self.product_list, self.monte)
+
+        for i in set(self.graph_dict.keys()):
+            s_set = [set() for _ in range(self.num_product)]
+            s_set[0].add(i)
+            ep = 0.0
+            for _ in range(self.monte):
+                ep += diff_ss.getSeedSetProfit(s_set)
+            mg = round(ep / self.monte, 4)
+
+            if mg > 0:
+                celf_item = (mg, 0, i, 0)
+                heap.heappush_max(celf_heap, celf_item)
+
+        return celf_heap
